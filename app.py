d61cc23fcd412d375962f6ca81b217d4b69ae9d4
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-06-21 17:06:51 -0500

    creando endpoint con metodo POST

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask # 1.importando libreria
+from flask import jsonify
 
 app = Flask(__name__)   # 2.instanciando flask
 
-# http://127.0.0.1:5000/list
-@app.route('/list', methods=['GET']) # 4.Creando endpoint
+# http://127.0.0.1:5000/getdata
+@app.route('/getdata', methods=['GET']) # 4.Creando endpoint
 def getdata():
     return "Hola Mundo"
 
+# http://127.0.0.1:5000/postdata
+@app.route('/postdata', methods=['POST']) # 4.Creando endpoint
+def postdata():
+    return jsonify(response = 'Metodo post OK')
+
 if __name__ == '__main__': # 3.Creando el servidor
-    app.run()
+    app.run(debug=True)
