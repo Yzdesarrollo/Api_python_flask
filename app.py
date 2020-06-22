c0a6ff857dc1f01f563f3e02264bde882215aa4c
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-06-22 10:16:11 -0500

    api con todos los metodos HTTP basicos

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask # 1.importando libreria
-from flask import jsonify
+from flask import jsonify #
 
 app = Flask(__name__)   # 2.instanciando flask
 
@@ -12,6 +12,16 @@ def getdata():
 @app.route('/postdata', methods=['POST']) # 4.Creando endpoint
 def postdata():
     return jsonify(response = 'Metodo post OK')
+
+# http://127.0.0.1:5000/updatedata
+@app.route('/updatedata', methods=['PUT']) # 4.Creando endpoint
+def updatedata():
+    return jsonify(response = 'Metodo put OK')
+
+# http://127.0.0.1:5000/deletedata
+@app.route('/deletedata', methods=['DELETE']) # 4.Creando endpoint
+def deletedata():
+    return jsonify(response = 'Metodo delete OK')
 
 if __name__ == '__main__': # 3.Creando el servidor
     app.run(debug=True)
