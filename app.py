76156d60c82c9e8cf62579f800bc70f10907d3d5
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-06-22 14:06:27 -0500

    todos los metodos HTTP funcionando

--- app.py
+++ app.py
@@ -14,7 +14,13 @@ app = Flask(__name__)   # 2.instanciando flask
 # http://127.0.0.1:5000/read
 @app.route('/read', methods=['GET']) # 4.Creando endpoint
 def read():
-    return "Hola Mundo"
+    sql = "SELECT * FROM tasks"
+    cur = mydb.cursor()
+    cur.execute(sql)
+    result = cur.fetchall()
+    print(result)
+    return jsonify(data = result)
+    #return render_template('home.html');
 
 # http://127.0.0.1:5000/create
 @app.route('/create', methods=['POST']) 
@@ -29,14 +35,29 @@ def create():
     print('task=> ', task, 'date=> ', date)
     return jsonify(response = 'Metodo post OK')
 
-# http://127.0.0.1:5000/updatedata
+# http://127.0.0.1:5000/update
 @app.route('/update', methods=['PUT']) 
 def updatedata():
+    data = request.get_json()
+    id = data['id']
+    task = data['task']
+    date = data['date']
+    sql = f"UPDATE tasks SET task = '{task}', date = '{date}' WHERE id = {id}"
+    cur = mydb.cursor()
+    cur.execute(sql)
+    mydb.commit()
+    print(task, date)
     return jsonify(response = 'Metodo put OK')
 
-# http://127.0.0.1:5000/deletedata
+# http://127.0.0.1:5000/delete
 @app.route('/delete', methods=['DELETE']) 
 def deletedata():
+    data = request.get_json()
+    id = data['id']
+    sql = f"DELETE FROM tasks WHERE id={id}"
+    cur = mydb.cursor()
+    cur.execute(sql)
+    mydb.commit()
     return jsonify(response = 'Metodo delete OK')
 
 if __name__ == '__main__': # 3.Creando el servidor
